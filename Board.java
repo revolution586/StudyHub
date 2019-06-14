3c5fc01e845c62484cdd904ae5370d05a9873105
mastercoder <48371762+revolution586@users.noreply.github.com>
2019-06-14 15:04:56 -0400

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,4 +1,14 @@
 package StudyHub;
 import javax.swing;
 import java.awt.Graphics2D;
-import 
+import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.JFrame;
+
+public class Board extends JFrame {
+  Application ex = new Application;
+	initUI();
+	int x = movementSpeed;
+	int y = initializeBox;
+}
