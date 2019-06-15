370f9e010105ce08eea209aed99ae0dca302d060
mastercoder <48371762+revolution586@users.noreply.github.com>
2019-06-15 18:25:43 -0400

    Update StudyHub.java

--- StudyHub.java
+++ StudyHub.java
@@ -1,1 +1,20 @@
+package StudyHub;
 
+import java.event.KeyEvent;
+import javax.swing.JFrame;
+
+public class StudyHub extends JFrame [
+  add (new Board);
+  init();
+  
+  setSize(380, 480);
+  DefaultCloseOperation (EXIT_ON_CLOSE);
+  setTitle ("StudyHub");
+  }
+ 
+private void initUI() {
+  
+  Application ex = new Application;
+}
+  
+  
