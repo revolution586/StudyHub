9b399d56cb9d6b5130da82a5783bdbccadd5336d
mastercoder <48371762+revolution586@users.noreply.github.com>
2019-06-18 17:55:00 -0400

    Update Board.java

--- Board.java
+++ Board.java
@@ -6,15 +6,10 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.JFrame;
 
-public class Board extends JFrame {
-  Application ex = new Application;
-	initUI();
-	int x = movementSpeed;
-	int y = initializeBox;
+public class Commons {
+	private int float BOARD_WIDTH = 200;
+	private int float BOARD_HEIGHT = 200;
+	private int float BORDER_RIGHT = 200;
+	private int float BORDER_LEFT = 200;
 }
 
-private static void main(String args[]) {
-	KeyPressed (KeyEvent e)
-		KeyEvent.KeyPressed("Space") = Exit.Application
-	// This sort of like sudo code, where I'm trying to piece together what variables are to be created //
-	// I will eventually insert visuals into GitHub to show my idea more clearly //
