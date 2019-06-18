210204570da932921e81d2311382309857b655e0
mastercoder <48371762+revolution586@users.noreply.github.com>
2019-06-18 18:03:41 -0400

    More Brainstorming
    So far, I have brainstormed more about the variables I want to set for this game. These variables being:
     - Time Studied
     - Graphics (including border variables)
     - Tables

--- Board.java
+++ Board.java
@@ -1,1 +1,35 @@
+package StudyHub;
 
+import javax.swing.JFrame;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyAdapter;
+import javax.swing.JPanel;
+
+public class Board extends JFrame implements Commons {
+  
+  public Board() {
+    
+    Octagon button = new Octagion();
+    setSize(380, 480);
+    int x = timeStudied;
+  }
+}
+
+private void timeStudied {
+  Coords co {
+     #Time Studied #XP Earned #
+     ####          ####
+  }
+  
+  public int setTable;
+  public int tableBorder;
+  public int tableContents;
+  
+  if (x > 25) {
+    System.out.println(" You have studied for an additional: 25" + 1)
+  }
+
+
+     
+
+    
