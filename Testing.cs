c358a9294751ee747fedbc0e6213719e30951a0c
stylistfanatical <48371762+revolution586@users.noreply.github.com>
2020-07-21 11:27:07 -0700

    Update Testing.cs

--- Testing.cs
+++ Testing.cs
@@ -3,4 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Testing : MonoBehaviour {
+  
+  [SerializeField] private LevelWindow levelWindow;
+  [SerializeField] private Player player;
+  [SerializeField] private EquipWindow equipWindow;
+  
+  private void Awake() {
+    LevelSystem levelSystem = new LevelSystem();
+    levelWindow.SetLevelSystem(levelSystem);
+    equipWindow.SetLevelSystem(levelSystem);
+    
+    LevelSystemAnimated levelSystemAnimated = new LevelSystemAnimted(levelSystem);
+    levelWindow.SetLevelSystemAnimated(levelSystemAnimated);
+    player.SetLevelSystemAnimated(levelSystemAnimated);
+  }
+}
 
