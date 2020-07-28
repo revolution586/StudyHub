d1e83e7b3354b332c7093e282f0f7ae250ea8c02
stylistfanatical <48371762+revolution586@users.noreply.github.com>
2020-07-27 23:35:42 -0700

    Update Timer.cs

--- Timer.cs
+++ Timer.cs
@@ -3,6 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Timer : MonoBehaviour
-{
+public class Timer : MonoBehaviour {
+	
+	Text text;
+	float theTime;
+	public float speed = 1;
+	bool playing;
+	
+	// Start is called before the first frame update //
+	void Start()
+	{
+		text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if (playing == true)
+		{
+			theTime += Time.deltaTime * speed;
+			string hours = Mathf.Floor((theTime % 216000) / 3600).ToString("00");
+			string minutes = Mathf.Floor((theTime % 3600) / 60).ToString("00");
+			string seconds = (theTime % 60).ToString("00");
+			text.text = hours + ":" + minutes + ":" + seconds;
+		}
+	}
+	
+	public void ClickPlay()
+	{
+		playing = true;
+	}
+	
+	public void ClickStop()
+	{
+		playing = false;
+	}
+	
+	public void ClickRestart()
+	{
+		bool resetted = true;
+		if (resetted)
+		{
+			text.text = "00:00:00";
+			theTime = 0;
+		}
+	}
+}
 	
