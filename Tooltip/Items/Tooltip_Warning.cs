5ba99da77c544cdf329ada45961a96752df88f67
stylistfanatical <48371762+revolution586@users.noreply.github.com>
2020-08-07 12:02:18 -0700

    Update Tooltip_Warning.cs

--- Tooltip/Items/Tooltip_Warning.cs
+++ Tooltip/Items/Tooltip_Warning.cs
@@ -1,1 +1,8 @@
-swqwqrqeqrqwrwq
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CodeMonkey.Utils;
+
+public class Tooltip_Warning : MonoBehaviour {
