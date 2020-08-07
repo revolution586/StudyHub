d9532b4791ed3fe9438e56db2d56a198138cbab5
stylistfanatical <48371762+revolution586@users.noreply.github.com>
2020-08-07 12:26:22 -0700

    Update Window.cs

--- Tooltip/Items/Window.cs
+++ Tooltip/Items/Window.cs
@@ -4,4 +4,34 @@ using UnityEngine;
 using CodeMonkey.Utils;
 
 public class Window : MonoBehaviour {
+	
+	private float attackCooldown;
+	
+	private void Start() {
+		
+		
+		Tooltip.AddTooltip(transform.Find("attackTooltipBtn"), "Attack damage");
+		Tooltip.AddTooltip(transform.Find("speedTooltipBtn"), "Movement speed");
+		Tooltip.AddTooltip(transform.Find("healthTooltipBtn"), "Health amount");
+		
+		Tooltip.AddTooltip(transform.Find("patrolBtn"), "Patrol");
+		Tooltip.AddTooltip(transform.Find("defendBtn"),"Defend");
+		
+		Tooltip.AddTooltip(transform.Find("attackBtn"), () => "Attack", " + (Mathf.Round(attackCooldown * 100f) / 100f));
+						   
+						   transform.Find("attackBtn").GetComponent<Button_UI>().ClickFunc = () => {
+							   if (attackCooldown > 0) {
+								   Tooltip_Warning.ShowTooltip_Static("Cannot attack!");
+							   } else {
+								   attackCooldown = 5f;
+							   }
+						   };
+						   }
+						   
+						   private void Update() {
+							   attackCooldown -= Time.deltaTime;
+							   if (attackCooldown < 0) attackCooldown = 0f;
+						   }
+						   }
+						   
 
