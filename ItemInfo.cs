5e07e3ee327678e75d7b9418819171197b75bc50
stylistfanatical <48371762+revolution586@users.noreply.github.com>
2020-08-07 11:13:25 -0700

    Update ItemInfo.cs

--- ItemInfo.cs
+++ ItemInfo.cs
@@ -1,1 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class ItemInfo : MonoBehaviour {
+	
+	public Sprite sprite;
+	public string itemName;
+	public string itemDescription;
+	public int STR;
+	public int DEX;
+	public int CON;
+	}
+	
