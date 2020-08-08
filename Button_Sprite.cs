2149217d6ecaff35e81467ebd989afd443505fc3
stylistfanatical <48371762+revolution586@users.noreply.github.com>
2020-08-07 22:35:50 -0700

    Update Button_Sprite.cs

--- Button_Sprite.cs
+++ Button_Sprite.cs
@@ -51,12 +51,12 @@ namespace MeatRobot.Utils {
   
   private Action internalOnMouseDownFunc, internalOnMouseEnterFunc, internalOnMouseExitFunc;
   
- #if SOUND_MANAGER
-  public Sound_Manager.Sound mouseOverSound, mouseClickSound;
-  #endif
-  #if CURSOR_MANAGER
-    public CursorManager.CursorType cursorMouseOver, cursorMouseOut;
-  #endif
+ // #if SOUND_MANAGER
+  // public Sound_Manager.Sound mouseOverSound, mouseClickSound;
+  // #endif
+  // #if CURSOR_MANAGER
+    // public CursorManager.CursorType cursorMouseOver, cursorMouseOut;
+  // #endif
   
   public void SetHoverBehaviourChangeColor(Color colorOver, Color colorOut) {
     hoverBehaviourType = HoverBehaviour.Change_Color;
@@ -67,6 +67,123 @@ namespace MeatRobot.Utils {
     SetupHoverBehaviour();
     }
     void OnMouseDown() {
+      if (!clickThroughUI && IsPointOverUI()) return; // Over UI!
+      
+      if (internalOnMouseDownFunc != null) internalOnMouseDownFunc();
+      if (ClickFunc != null) ClickFunc();
+      if (triggerMouseOutFuncOnClick) OnMouseExit();
+      }
+     public void Manual_OnMouseExit() {
+        OnMouseExit();
+      }
+      void OnMouseUp() {
+        if (MouseUpOnceFunc != null) MouseUpOnceFunc();
+      }
+      void OnMouseEnter() {
+        if (!clickThroughUI && IsPointerOverUI()) return;
+        
+        if (internalOnMouseEnterFunc != null) internalOnMouseEnterFunc();
+        if (hoverBehaviour_Move) transform.localPosition = posEnter;
+        if (hoverBehaviourFunc_Enter != null) hoverBehaviourFunc_Enter();
+        if (MouseOverOnceFunc != null) MouseOverOnceFunc();
+        if (MouseOverOnceTooltipFunc != null) MouseOverOnceTooltipFunc();
+        
+       }
+       void OnMouseExit() {
+        if (internalOnMouseExitFunc != null) internalOnMouseExitFunc();
+        if (hoverBehavuour_Move) transform.localPosition = posExit;
+        if (hoverBehaviourFunc_Exit != null) hoverBehaviourFunc_Exit();
+        if (MouseOutOnceFunc != null) MouseOutOnceFunc();
+        if (MouseOutOnceTooltipFunc != null) MouseOutOnceTooltipFunc();
+       }
+       
+        void OnMouseOver() {
+          if (!clickThroughUI && IsPointerOverUI()) return;
+          
+          if (Input.GetMouseButton(1)) {
+            if (MouseRightDownFunc != null) MouseRightDownFunc();
+            if (!draggingMouseRight && triggerMouseRightDragOnEnter) {
+              draggingMouseRight = true;
+              mouseRightDragStart = GetWorldPositionFromUI();
+            }
+          }
+          if (Input.GetMouseButtonDown(1)) {
+            draggingMouseRight = true;
+            mouseRightDragStart = GetWorldPositionFromUI();
+            if (MouseRightDownOnceFunc != null) MouseRightDownOnceFunc();
+          }
+        }
+  void Update() {
+    if (draggingMouseRight) {
+      if (MouseRightDragUpdateFunc != null) MouseRightDragUpdateFunc(mouseRightDragStart, GetWorldPositionFromUI());
+    }
+    if (Input.GetMouseButtonUp(1)) {
+      if (draggingMouseRight) {
+        draggingMouseRight = false;
+        if (MouseRightDragFunc != null) MouseRightDragFunc(mouseRightDragStart, GetWorldPositionFromUI());
+      }
+      if (MouseRightUpFunc != null) MouseRightUpFunc();
+    }
+  }
+  
+  void Awake() {
+    if (GetWorldCamera = null) SetGetWorldCamera(() => Camera.main);
+    posExit = transform.localPosition;
+    posEnter = transform.localPosition + (Vector3)hoverBehaviour_Move_Amount;
+    SetupHoverBehaviour();
+    
+   //#if SOUND_MANAGER
+      // Sound Manager
+     // internalOnMouseDownFunc += () => { if (mouseClickSound != Sound_Manager.Sound.None) Sound_Manager.PlaySound(mouseClickSound); };
+      // internalOnMouseEnterFunc += () => { if (mouseOverSound != Sound_Manager.Sound.None) Sound_Manager.PlaySound(mouseOverSound); };
+    // #endif
+    
+    // #if CURSOR_MANAGER
+      // Cursor Manager
+      // internalOnMouseExitFunc += () => { if (cursorMouseOut != CursorManager.CursorType.None) CursorManager.SetCursor(cursorMouseOut); };
+      // internalOnMouseEnterFunc += () => { if (cursorMouseOver != CursorManager.CursorType.None) CursorManager.SetCursor(cursorMouseOver); };
+    //#end if
+  }
+  private void SetupHoverBehaviour() {
+    switch (hoverBehaviourType) {
+      case HoverBehaviour.Change_Color:
+        hoverBehaviourFunc_Enter = delegate () { hoverBehaviour_Image.color = hoverBehaviour_Color_Enter; };
+        hoverBehaviourFunc_Exit = delegate () { hoverBehaviour_Image.color = hoverBehaviour_Color_Exit; };
+        break;
+      case HoverBehaviour.Change_Image:
+        hoverBehaviourFunc_Enter = delegate () { hoverBehaviour_Image.sprite = hoverBehaviour_Sprite_Enter; };
+        hoverBehaviourFunc_Exit = delegate () { hoverBehaviour_Image.sprite = hoverBehaviour_Sprite_Exit; };
+        break;
+      case HoverBehaviour.Change_SetActive:
+        hoverBehaviourFunc_Enter = delegate () { hoverBehaviour_Image.gameObject.SetActive(true); };
+        hoverBehaviourFunc_Exit = delegate () { hoverBehaviour_Image.gameObject.SetActive(false); };
+        break;
+    }
+  }
+  
+  
+  
+  private static Vector3 GetWorldPositionFromUI() {
+    Vector3 worldPosition = GetWorldCamera().ScreenToWorldPoint(Input.mousePosition);
+    return worldPosition;
+  }
+  private static bool IsPointerOverUI() {
+    if (EventSystem.current.IsPointerOverGameObject()) {
+      return true;
+    } else {
+      PointerEventData pe = new PointerEventData(EventSystem.current);
+      pe.position = Input.mousePosition;
+      List<RaycastResult> hits = new List<RaycastResult>();
+      EventSystem.current.RaycastAll(pe, hits);
+      return hits.Count > 0;
+    }
+  }
+}
+}
+                                                
+        
+        
+        
     
   
   
